9de75a12967bb346167f7ab27e49bc5fab792a65
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-06-13 21:10:14 +0100

    refactor: Refactor Table component

--- src/modules/dashboard/components/table/Table.tsx
+++ src/modules/dashboard/components/table/Table.tsx
@@ -1,15 +1,8 @@
 import TableRow from "./TableRow";
 import TableHead from "./TableHead";
+import { Airline } from "./types/types";
 
-const rowData = [
-  { id: 1, name: "name" },
-  { id: 2, name: "name" },
-  { id: 3, name: "name" },
-  { id: 4, name: "name" },
-  { id: 5, name: "name" },
-];
-
-export default function Table({ airlines }: { airlines: any }) {
+export default function Table({ airlines }: { airlines: Airline[] }) {
   return (
     <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100">
       <h2 className="mb-4 text-2xl font-semibold leading-tight">Data</h2>
@@ -17,28 +10,12 @@ export default function Table({ airlines }: { airlines: any }) {
       <br />
       <div className="overflow-x-auto">
         <table className="w-full p-6 text-xs text-left whitespace-nowrap">
-          {/* <colgroup>
-            <col className="w-5" />
-            <col />
-            <col />
-            <col />
-            <col />
-            <col />
-            <col />
-            <col className="w-5" />
-          </colgroup> */}
           <TableHead />
-          <br />
+
           <tbody>
-            <TableRow rowData={rowData} />
-          </tbody>
-          <br />
-          <tbody>
-            <TableRow rowData={rowData} />
-          </tbody>
-          <br />
-          <tbody>
-            <TableRow rowData={rowData} />
+            {airlines.map((airline: Airline) => {
+              return <TableRow key={airline.flight.icao} airline={airline} />;
+            })}
           </tbody>
         </table>
       </div>
