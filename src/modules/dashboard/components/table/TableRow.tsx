9c4bae7611e146909afad55e3241758504b60a72
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-06-13 21:10:29 +0100

    refactor: Refactor TableRow component

--- src/modules/dashboard/components/table/TableRow.tsx
+++ src/modules/dashboard/components/table/TableRow.tsx
@@ -1,23 +1,40 @@
 import Dropdown from "../../../common/components/dropdown/Dropdown";
+import { formatDate, formatTime } from "./helpers/helpers";
+import { Airline } from "./types/types";
 
-interface RowData {
-  id: number;
-  name: string;
-}
+export default function TableRow({ airline }: { airline: Airline }) {
+  const {
+    flight_date,
+    flight_status,
+    airline: { name },
+    departure,
+    arrival,
+  } = airline;
 
-export default function TableRow({ rowData }: { rowData: RowData[] }) {
   return (
-    <tr className="text-lg">
-      {rowData.map(({ id, name }) => {
-        return (
-          <td key={id} className="text-center border-2">
-            <p>{name}</p>
-          </td>
-        );
-      })}
-      <td className="border-2 text-center">
-        <Dropdown />
-      </td>
-    </tr>
+    <>
+      <tr className="text-lg">
+        <td className="text-center border-2">
+          <p>{formatDate(flight_date)}</p>
+        </td>
+        <td className="text-center border-2">
+          <p>{flight_status}</p>
+        </td>
+        <td className="text-center border-2">
+          <p>{name}</p>
+        </td>
+        <td className="text-center border-2">
+          <p>{formatTime(departure.estimated)}</p>
+        </td>
+        <td className="text-center border-2">
+          <p>{formatTime(arrival.estimated)}</p>
+        </td>
+        <td className="border-2 text-center">
+          <Dropdown />
+        </td>
+      </tr>
+    </>
   );
 }
+
+
